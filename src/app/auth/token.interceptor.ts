5dbd0ff230c58136722c70665ef8c30b62526f48
Bharat Singh <bksingh@g.cofc.edu>
2020-07-14 10:36:55 -0400

    Fixing compile error

--- src/app/auth/token.interceptor.ts
+++ src/app/auth/token.interceptor.ts
@@ -5,8 +5,10 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { AuthService } from './auth/auth.service';
-import { Observable } from 'rxjs/Observable'; @Injectable()
+import { Observable } from 'rxjs/internal/Observable';
+//import { AuthService } from './auth/auth.service';
+
+@Injectable()
 export class TokenInterceptor implements HttpInterceptor {
     constructor() { } intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
