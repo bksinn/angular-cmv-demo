df0891ea325702441da82bf43fbe9b6ba733aa52
Patrick Sarnowski <psarnowski@gmail.com>
2020-07-14 12:24:07 -0400

    Implement AuthGuard.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, CanActivate } from '@angular/router';
 import { PhonesComponent } from './phones/phones.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { ForgotUsernameComponent } from './forgot-username/forgot-username.component';
+import { AuthGuardService as AuthGuard} from './auth/auth-guard.service';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'logout', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'phones', component: PhonesComponent },
-  { path: 'forgot-password', component: ForgotPasswordComponent },
-  { path: 'forgot-username', component: ForgotUsernameComponent }
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard]  },
+  { path: 'phones', component: PhonesComponent, canActivate: [AuthGuard]  },
+  { path: 'forgot-password', component: ForgotPasswordComponent, canActivate: [AuthGuard]  },
+  { path: 'forgot-username', component: ForgotUsernameComponent, canActivate: [AuthGuard]  }
 ];
 
 @NgModule({
